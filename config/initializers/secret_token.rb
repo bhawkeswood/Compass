5053778f5036cca6f6ee2903a4ea9973c35c7117
bhawkeswood <bhawkeswood@gmail.com>
2013-10-07 12:07:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Compass::Application.config.secret_key_base = '74dc52783ee3fd4fb9bc27ba796e2d52f0e4a97b3fee104141c37cb177c8d49c745a29f69a87dccace9a2eb51e90fb5f763aabb785cdd026d6dfdc8a84a19e2b'
