4cc777ceb668bb0be136dcb998f7b6af3ec38a5d
kimyee <kimyee.16@gmail.com>
2013-10-08 21:43:13 -0400

    generate dropdown menu for new deal form and attempt to add datepicker(code not yet working)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131008204258) do
+ActiveRecord::Schema.define(version: 20131008234651) do
 
   create_table "deals", force: true do |t|
     t.string   "project_name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20131008204258) do
     t.string   "description"
     t.boolean  "done"
     t.integer  "deal_id"
+    t.string   "due_date"
   end
 
   create_table "users", force: true do |t|
