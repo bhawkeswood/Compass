df214338898935a89c48034da7ee281c7e9b6534
kimyee <kimyee.16@gmail.com>
2013-10-10 15:08:09 -0400

    Add validations for required fields in deal.rb, add hint under developer field in _form.html.erb, and change Back to Back to To Do List in new.html.erb

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,4 +2,8 @@ class Deal < ActiveRecord::Base
 	belongs_to :user
 	has_many :tasks
 	accepts_nested_attributes_for :tasks, :allow_destroy => true
+
+	validates :project_name, presence: true
+	validates :unit_number, presence: true
+	validates :developer, presence: true
 end
