36ca9be9816fce4f910115fe259972805a67baec
kimyee <kimyee.16@gmail.com>
2013-10-09 21:02:32 -0400

    Use typeahead for task field requirement

--- app/assets/javascripts/deal.js
+++ app/assets/javascripts/deal.js
@@ -3,5 +3,13 @@
 $(function(){
 	$('#tasks').on("cocoon:after-insert", function(){
 		$('div#task_datepicker input').datepicker();
+		$('div#req_description input').typeahead({
+			name: 'description',
+			local: ["Commitment Letter", 
+							"Deposit Cheques", 
+							"Firm Date", 
+							"Mortgage Pre-Approval", 
+							"Trade Record"]
+		});
 	});
 });
