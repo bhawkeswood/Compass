418d75eefbff3005f2ed39858751f2cbff3876d6
bhawkeswood <bhawkeswood@gmail.com>
2013-10-09 22:36:15 -0400

    Creating the welcome controller and index view for sign in up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Compass::Application.routes.draw do
+  get "welcome/index"
   resources :deals
 
-  root to: 'deals#index'
+  root to: 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
