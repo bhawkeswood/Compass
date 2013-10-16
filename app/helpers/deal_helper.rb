5dbe965a066d19b741e944d08834479050fd49d2
bhawkeswood <bhawkeswood@gmail.com>
2013-10-16 19:15:39 -0400

    Enabling the index view to have a different message when no tasks are present

--- app/helpers/deal_helper.rb
+++ app/helpers/deal_helper.rb
@@ -8,4 +8,14 @@ module DealHelper
     end
   end 
 
+  def all_deals(deals)
+    deals.each do |deal|
+      if deal.incomplete_tasks.length == 0
+        return "<h1>Yay!  You're done!</h1>".html_safe
+      else
+        return "<h1>Incomplete Tasks:</h1>".html_safe
+      end
+    end
+  end
+
 end
