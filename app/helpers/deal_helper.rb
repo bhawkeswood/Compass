c9299d9a91e3b8b2585ffb18086fa4d27f881705
bhawkeswood <bhawkeswood@gmail.com>
2013-10-16 18:03:24 -0400

    Updating the navbar to show incomplete tasks and add icons

--- app/helpers/deal_helper.rb
+++ app/helpers/deal_helper.rb
@@ -1,2 +1,11 @@
 module DealHelper
+
+  def sum_of_incomplete_tasks(deals)
+    if deals != nil
+      deals.inject(0) do |all_inc_tasks, deal|
+        all_inc_tasks + deal.incomplete_tasks.length
+      end
+    end
+  end 
+
 end
