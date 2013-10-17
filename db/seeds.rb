b995a8aa9a54b4fff55b23b50c974e60358ef401
kimyee <kimyee.16@gmail.com>
2013-10-17 16:47:05 -0400

    Seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 User.destroy_all
 Deal.destroy_all
 
-user = User.create! (first_name: "Jane", last_name: "Doe", email: "jane@example.com", password: "password")
+user = User.create!(first_name: "Jane", last_name: "Doe", email: "jane@example.com", password: "password")
 
 10.times do |i|
 	deal1 = user.deals.create!(project_name: "Project Name #{i}", developer: "Developer #{i}", unit_number: "Unit Number #{i}")
