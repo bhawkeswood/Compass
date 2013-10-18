85aad8dfe50756148976ce599e003c0f59b9be32
bhawkeswood <bhawkeswood@gmail.com>
2013-10-17 20:59:21 -0400

    Attempting to add mailer for reminders

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -17,6 +17,7 @@ class DealsController < ApplicationController
     @deal = Deal.new deal_params
     @user = current_user
     if @deal.save
+      time_offset
       ModelMailer.event_creation_notification(@user, @deal).deliver
       redirect_to @deal
     else
@@ -52,6 +53,15 @@ class DealsController < ApplicationController
   def deal_params
     params.require(:deal).permit(:project_name, :developer, :unit_number,
      :user_id, tasks_attributes: [:id, :description, :done, :_destroy, :due_date], commissions_attributes: [:id, :description, :amount, :commission_date, :done, :_destroy, :installment])
+  end
+
+  def time_offset
+        @deal.incomplete_tasks do |task|
+          if (Time.now - task.due_date) < 1.day
+              ModelMailer.event_reminder_notification(@user, @deal).deliver
+          end
+        end
+
   end
 
 end
