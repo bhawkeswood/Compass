b2d0832e3e4d7ebc6bdbc109045554215e5aa86c
kimyee <kimyee.16@gmail.com>
2013-10-18 13:35:11 -0400

    Update format of commission amount

--- app/models/commission.rb
+++ app/models/commission.rb
@@ -4,4 +4,9 @@ class Commission < ActiveRecord::Base
 
   scope :complete, -> { where(done: true) }
   scope :incomplete, -> { where(done: false) }
+
+  def formatted_price
+		price_in_dollars = amount.to_f/ 1
+		sprintf("%.2f", price_in_dollars)  	
+	end 
 end
