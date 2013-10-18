43887ab749db9d2c85303d821d61f01a8cf11f0f
kimyee <kimyee.16@gmail.com>
2013-10-18 17:58:33 -0400

    Edit deal helper

--- app/helpers/deal_helper.rb
+++ app/helpers/deal_helper.rb
@@ -10,20 +10,20 @@ module DealHelper
 
   def all_tasks(deals)
     deals.each do |deal|
-      if deal.incomplete_tasks.empty? == true
+      if deal.incomplete_tasks.empty? == false
         return "<h1>Incomplete Tasks</h1>".html_safe
       else
-        return "<h1>Incomplete Tasks</h1>".html_safe
+        return "<h1>Yay! You're done!</h1>".html_safe
       end
     end
   end
 
   def all_commissions(deals)
     deals.each do |deal|
-      if deal.unpaid_commissions.empty? == true
+      if deal.unpaid_commissions.empty? == false
         return "<h1>Unpaid Commissions</h1>".html_safe
       else
-        return "<h1>Unpaid Commissions</h1>".html_safe
+        return "<h1>Yay! You Got Paid!</h1>".html_safe
       end
     end
   end
